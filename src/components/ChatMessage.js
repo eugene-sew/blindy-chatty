7a531220490b2a3acb212b0b165ba4c7a0ae0a66
Eugene Sewor <eugenesew@gmail.com>
2022-02-11 02:58:55 +0000

    error rectified

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,12 +1,11 @@
 import { auth } from "../firebase";
 
 function ChatMessage(props) {
-  const { text, uid, photoUrl } = props.message;
+  const { text, uid } = props.message;
   const messageClass = uid === auth.currentUser.uid ? "sent" : "recieved";
   return (
     <div className="grid mt-5">
       <div className={`message ${messageClass}`}>
-        {/* <img src={photoUrl} /> */}
         <p>{text}</p>
       </div>
     </div>
